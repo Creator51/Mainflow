a5dc83398ae28aae63be61e3a6dc223d4b3a2e12
Creator51 <164214553+Creator51@users.noreply.github.com>
2024-07-18 21:36:58 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,9 @@
+#Create a List
+list=[1,2,3,4,5,6,7,8]
+print(f"Orginal List {list}")
+list.append(9) #Adding An Element to the List
+print(f"Updated List {list}")
+list[0]=100 # Changing an element in the List
+print(f"Updated List {list}")
+list.pop() #Removing an Element from the List
+print(f"Updated List {list}")

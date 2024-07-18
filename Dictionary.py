92925006a99524b4936affc78fe28410f4caef35
Creator51 <164214553+Creator51@users.noreply.github.com>
2024-07-18 21:44:18 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+# Creating a Dictionary 
+dict={1:'Apple',2:'Mango',3:'Banana'}
+print(f"Orginal Dictionary {dict}")
+print(f"Accessing an Element in Dictionary {dict[1]}") # Accessing Elements in Dictionary
+dict[4]='Orange' # Adding an Element in Dictionary
+print(f"Updated Dictionary {dict}")
+dict[1]='Cat' # Modifing An Element
+print(f"Updated Dictionary {dict}")
+del dict[2] # Removing An Element from the Dictionary
+print(f"Updated Dictionary {dict}")
---
+++ Set.py
@@ -0,0 +1,9 @@
+# Creating a Set
+set={1,2,3,4,5,6,7}
+print(f"Orginal set {set}")
+set.add(8) # Adding an Element into the set
+print(f"Updated Set {set}")
+set={1,2,2,2,3,3,4,5,5,6,6,7,7} # Set Cannot contain 2 same Elements
+print(f"Updated Set {set}")
+set.remove(1) # Removing an Element from the set
+print(f"Updated Set {set}")
